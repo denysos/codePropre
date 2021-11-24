cbeba965661a5425cfc7d77055e6fcba886a1c2d
denis <denis.mourier2@gmail.com>
2021-11-24 12:28:59 +0100

    TP netoyage ex2

---
+++ src/main/java/ex2/CompteCourant.java
@@ -0,0 +1,50 @@
+package ex2;
+
+
+/**
+ * Type Compte Courant
+ * @author denis
+ * @param solde
+ * @param decouvert
+ *
+ */
+public class CompteCourant extends CompteBancaire {
+	
+	/**
+	 * @param solde
+	 * @param decouvert
+	 */
+	
+	/** decouvert : un découvert est autorisé seulement pour les comptes courants */
+	private double decouvert;
+
+	public CompteCourant(double solde, double decouvert) {
+		super(solde, "CC");
+		this.decouvert = decouvert;
+	}
+
+	public void debiterMontant(double montant) {
+		if (this.solde - montant > decouvert) {
+			this.solde = solde - montant;
+		}
+	}
+
+	/**
+	 * Getter for decouvert
+	 * 
+	 * @return the decouvert
+	 */
+	public double getDecouvert() {
+		return decouvert;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param decouvert the decouvert to set
+	 */
+	public void setDecouvert(double decouvert) {
+		this.decouvert = decouvert;
+	}
+
+}
--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,53 @@
 package ex2;
 
+/**
+ * code bancaire de type compte Livret A
+ * 
+ * @author denis
+ *
+ */
 public class LivretA extends CompteBancaire {
+	
+	/**
+	 * @param solde
+	 * @param tauxRemuneration
+	 */
+	
+	private double tauxRemuneration;
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde, "LA");
+		this.tauxRemuneration = tauxRemuneration;
+	}
+
+	public void appliquerRemuAnnuelle() {
+		this.solde = solde + solde * tauxRemuneration / 100;
+
+	}
+
+	public void debiterMontant(double montant) {
+		if (this.solde - montant > 0) {
+			this.solde = solde - montant;
+		}
+
+	}
+
+	/**
+	 * Getter for tauxRemuneration
+	 * 
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
 	}
 
 }
